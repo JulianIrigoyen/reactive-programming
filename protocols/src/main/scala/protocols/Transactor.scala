62f85f11ff8e180e0919cce0df0bc2d2b8526fdc
Julian Martin Irigoyen <julian.irigoyen@tiendanube.com>
2020-09-27 18:59:04 -0300

    WIP

--- protocols/src/main/scala/protocols/Transactor.scala
+++ protocols/src/main/scala/protocols/Transactor.scala
@@ -31,7 +31,7 @@ object Transactor {
     *                       terminating the session
     */
   def apply[T](value: T, sessionTimeout: FiniteDuration): Behavior[Command[T]] =
-      ???
+      SelectiveReceive(30, idle(value, sessionTimeout).narrow[Command[T]])//.narrow[Command[T]]
 
   /**
     * @return A behavior that defines how to react to any [[PrivateCommand]] when the transactor
@@ -54,7 +54,17 @@ object Transactor {
     *   - Messages other than [[Begin]] should not change the behavior.
     */
   private def idle[T](value: T, sessionTimeout: FiniteDuration): Behavior[PrivateCommand[T]] =
-    ???
+    Behaviors receivePartial {
+      case (ctx, Transactor.Begin(replyTo)) =>
+        val session = ctx.spawnAnonymous(sessionHandler(value, ctx.self, Set.empty))
+
+        //The Transactor uses DeathWatch to supervise each session, rolling back its effects upon failure.
+        ctx.watchWith(session, RolledBack(session))
+        ctx.setReceiveTimeout(sessionTimeout, RolledBack(session))
+        replyTo ! session
+        inSession(value, sessionTimeout, session)
+      case _ => Behaviors.ignore
+    }
 
   /**
     * @return A behavior that defines how to react to [[PrivateCommand]] messages when the transactor has
@@ -67,7 +77,15 @@ object Transactor {
     * @param sessionRef Reference to the child [[Session]] actor
     */
   private def inSession[T](rollbackValue: T, sessionTimeout: FiniteDuration, sessionRef: ActorRef[Session[T]]): Behavior[PrivateCommand[T]] =
-    ???
+    Behaviors receivePartial {
+      case (_, Transactor.Committed(session, value)) if sessionRef.equals(session) =>
+        idle(value, sessionTimeout)
+      case (ctx, Transactor.RolledBack(session)) if sessionRef.equals(session) =>
+        ctx stop session
+        idle(rollbackValue, sessionTimeout)
+
+      case (_, Begin(_)) => Behaviors.unhandled
+    }
 
   /**
     * @return A behavior handling [[Session]] messages. See in the instructions
@@ -78,6 +96,46 @@ object Transactor {
     * @param done Set of already applied [[Modify]] messages
     */
   private def sessionHandler[T](currentValue: T, commit: ActorRef[Committed[T]], done: Set[Long]): Behavior[Session[T]] =
-      ???
+      Behaviors.receivePartial {
+        case (_, Extract(f, replyTo)) =>
+          // apply the given projector function to the current Transactor value (possibly modified by the current session) and return the result to the given ActorRef;
+          // if the projector function throws an exception the session is terminated
+          replyTo ! f(currentValue)
+          Behaviors.same
 
+        case (ctx, Modify(f, id, reply, replyTo)) =>
+          //calculate a new value for the Transactor by applying the given function to the current value
+          // (possibly modified by the current session already) and return the given reply value
+          // to the given replyTo ActorRef; if the function throws an exception the session is terminated
+          // and all its modifications are rolled back;
+          // the id argument serves as a deduplication identifier:
+          //  sending the a Modify command with an id previously used within the current session
+          //  will not apply the modification function but send the reply immediately
+          (done, id) match {
+            //new modify op
+            case _ if !(done contains id) =>
+              //notify change
+              val modification = f(currentValue)
+              ctx.self ! Commit(modification ,replyTo)
+              //update state
+              sessionHandler(f(currentValue), commit, done + id)
+
+            //already done
+            case _ =>
+              replyTo ! reply
+              Behaviors.same
+          }
+
+        case (context, Commit(reply, replyTo)) =>
+          //terminate the current session, committing the performed modifications and thus making
+          // the modified value available to the next session;
+          // the given reply is sent to the given ActorRef as confirmation
+          replyTo ! reply
+          commit ! Committed(context.self, currentValue)
+          Behaviors.stopped
+
+        case (_, Rollback()) =>
+          Behaviors.stopped
+
+      }
 }
